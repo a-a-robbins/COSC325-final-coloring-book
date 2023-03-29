4b84bcc4de88f658be36fb2729c3f18d4cf05be4
a-a-robbins <a.a.robbins@outlook.com>
2023-03-29 08:22:02 -0500

    Add files via upload

---
+++ ColoringBook/ColoringBook/ColoringBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoringBookApp.swift
+//  ColoringBook
+//
+//  Created by ARobbins on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ColoringBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
